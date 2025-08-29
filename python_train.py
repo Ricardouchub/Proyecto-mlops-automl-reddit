70c3c7511744ff023490aaf1c1da2bdd59ad11e8
Ricardouchub <rockevans@gmail.com>
2025-08-28 22:06:17 -0400

    Update python_train.py

--- python_train.py
+++ python_train.py
@@ -1,31 +1,31 @@
-# 1. Importar las librerías necesarias
 import pandas as pd
-from pycaret.regression import *
+from pycaret.classification import *
 
-# 2. Cargar el conjunto de datos
-print("Cargando datos...")
-dataset = pd.read_csv('insurance.csv')
+# Carga  dataset
+print("Cargando datos de Churn...")
+dataset = pd.read_csv('churn_data.csv')
+# Eliminamos la columna de Customer ID ya que no es útil para la predicción
+dataset = dataset.drop('CustomerID', axis=1)
 print("Datos cargados exitosamente.")
 
-# 3. Configurar el entorno de PyCaret (¡AQUÍ ESTÁ LA MAGIA!)
-print("Configurando el experimento de regresión con MLflow...")
+# Configurar el experimento de clasificación
+print("Configurando el experimento de clasificación con MLflow...")
 s = setup(data=dataset, 
-          target='charges', 
+          target='Churn', 
           session_id=123,
-          log_experiment=True,                       # <-- CAMBIO: Activa el registro
-          experiment_name='insurance_cost_prediction', # <-- CAMBIO: Nombra el experimento
+          log_experiment=True,
+          experiment_name='churn_prediction', 
           verbose=False)
 print("Configuración completa.")
 
-# 4. Comparar todos los modelos
-print("Buscando el mejor modelo...")
+# Comparar modelos y encontrar el mejor
+print("Buscando el mejor modelo de clasificación...")
 best_model = compare_models()
 print("Mejor modelo encontrado:")
 print(best_model)
 
-# 5. Finalizar y guardar el modelo
-# Finalize_model re-entrena el modelo con todos los datos
+# Finalizar y guardar el modelo
 print("Finalizando y guardando el modelo...")
 final_model = finalize_model(best_model)
-save_model(final_model, 'best_insurance_model')
+save_model(final_model, 'churn_model')
 print("¡Modelo guardado! Proceso completado.")
